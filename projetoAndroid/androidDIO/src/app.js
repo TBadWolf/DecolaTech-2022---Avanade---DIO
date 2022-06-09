26548fe856fff29d7122e62c0e7df26274f20ede
TBadWolf <barbara.rce@hotmail.com>
2022-06-08 21:56:04 -0300

    Update app.js

--- projetoAndroid/androidDIO/src/app.js
+++ projetoAndroid/androidDIO/src/app.js
@@ -5,6 +5,9 @@ const colorGithub = '#010409';
 const colorfontGit = '#C9D1D9';
 const imageProfile = 'https://avatars.githubusercontent.com/u/104402057?v=4';
 const UrlPerfil = 'https://github.com/TBadWolf';
+const Linkedin = 'https://www.linkedin.com/in/barbara-esteves-6aa27a134/';
+const DIO = 'https://web.dio.me/users/barbara_rce?tab=achievements';
+
 
 const app = () => {
 
@@ -15,6 +18,24 @@ const app = () => {
         console.log('Link aprovado');
         console.log('Abrindo o link...');
           await Linking.openURL(UrlPerfil);
+       }
+    }
+       const handlePressGoToLinkedin = async () => {
+        console.log('Verificando link');
+       const res = await  Linking.canOpenURL (Linkedin);
+        if(res){
+        console.log('Link aprovado');
+        console.log('Abrindo o link...');
+          await Linking.openURL(Linkedin);
+       }
+    }
+    const handlePressGoToDIO = async () => {
+        console.log('Verificando link');
+       const res = await  Linking.canOpenURL (DIO);
+        if(res){
+        console.log('Link aprovado');
+        console.log('Abrindo o link...');
+          await Linking.openURL(DIO);
        }
     };
     return ( // os comentos abaixo como style = {} adicionam os objetos de estilo configurados abaixo, no barStyle altera a cor da barra de cima, selecionando a cor PINK no caso.
@@ -28,6 +49,16 @@ const app = () => {
             <Pressable onPress={handlePressGoToGitHub}>
             <View style={style.button}>
                 <Text style={[style.defaultText]}>Open in Github</Text>
+            </View>
+            </Pressable>
+            <Pressable onPress={handlePressGoToLinkedin}>
+            <View style={style.button}>
+                <Text style={[style.defaultText]}>Open in Linkedin</Text>
+            </View>
+            </Pressable>
+            <Pressable onPress={handlePressGoToDIO}>
+            <View style={style.button}>
+                <Text style={[style.defaultText]}>Open in DIO Profile</Text>
             </View>
             </Pressable>
         </View>
